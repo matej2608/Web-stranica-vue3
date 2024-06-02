83b01f1203689b5367d825f6434251cfe7ed8987
matej2608 <109682099+matej2608@users.noreply.github.com>
2024-06-02 22:18:42 +0200

    Initial commit

---
+++ resto-project/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <img class="logo" src="../assets/staripodrum.jpg">
+    <h1>Login</h1>
+    <div class="login">
+        <input type="text" v-model="email" placeholder="Enter Email" />
+        <input type="password" v-model="password" placeholder="Enter Password" />
+        <button v-on:click="login">Login</button>
+        <p>
+            <router-link to="/sign-up">Sign up</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Login',
+    data()
+    {
+        return{
+        email:'',
+        password:''
+
+    }
+    },
+
+    methods:{
+        async login()
+        {
+            let result = await axios.get(
+                `http://localhost:4000/users?email=${this.email}&password=${this.password}`
+            )
+        console.warn(this.email,this.password)
+        }
+    }
+
+};
+
+
+</script>
---
+++ resto-project/src/components/SignUp.vue
@@ -0,0 +1,60 @@
+<template>
+<img class="logo" src="../assets/staripodrum.jpg">
+<h1>SignUp</h1>
+<div class="register">
+    <input type="text" v-model="name" placeholder="Enter Name" />
+    <input type="text" v-model="email" placeholder="Enter Email" />
+    <input type="password" v-model="password" placeholder="Enter Password" />
+    <button v-on:click="signUp">Sign Up</button>
+    <p>
+        <router-link to="/login">Login</router-link>
+    </p>
+
+</div>
+</template>
+<script>
+import axios from 'axios'
+export default{
+    name:'SignUp',
+    data()
+    {
+        return {
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods: {
+        async signUp()
+        {
+            let result =await axios.post("http://localhost:4000/users",{
+                email:this.email,
+                password:this.password,
+                name:this.name
+            });
+
+            console.warn(result);
+            if(result.status==201)
+            {
+                
+                localStorage.setItem("user-info",JSON.stringify(result.data))
+                this.$router.push({name:'Home'})
+            }
+        }
+    },
+    mounted()
+    {
+        let user= localStorage.getItem('user-info');
+        if(user) 
+        {
+            this.$router.push({name:'Home'})  
+        }
+    }
+
+}
+   
+</script>
+
+<style>
+
+</style>
